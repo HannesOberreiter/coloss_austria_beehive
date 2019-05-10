1baa261ecf80c8c953c1178e9af36a4495937924
Hannes Oberreiter <office@btree.at>
2019-05-10 19:06:58 +0200

    + added yield plot

---
+++ 2019 R Files/Plot_Factor_Yield.R
@@ -0,0 +1,142 @@
+##############################
+##############################
+### Survey Bee Hive Losses ###
+# (c) 2019 Hannes Oberreiter #
+##############################
+##############################
+
+####### OPTERATION FACTOR YIELD ###########
+
+# Import Header
+source( "Partials_Header.r" )
+source( "Partials_Header_map.r" )
+
+# Import our Custom Functions
+source( "Partials_Functions.r" )
+
+#### START CODE #####
+
+# List of Factors we want in our Plot
+oList = list(
+  c("brassica_napus", "(A) Rapeseed"),
+  c("zea_mays", "(B) Maize"),
+  c("helianthus_annuus", "(C) Sunflower"),
+  c("late_catch_crop", "(D) Late Catch Crop"),
+  c("honeydew", "(E) Honeydew"),
+  c("melezitose", "(F) Melezitose")
+)
+
+# Create dummy Dataframe, to insert rows later
+D.FACTORS <- 
+  setNames( 
+    data.frame( matrix( ncol = 11, nrow = 0)), 
+    c( "ff", "c", "n", "hives_winter", "lost_a", "lost_b", "lost_c", "hives_lost_rate", "lowerlim", "middle", "upperlim")
+    )
+
+# Loop through list and create for all factors CI
+for( i in oList){
+  temp.n <- get( i[1], pos = D.FULL )
+  # remove "Unsicher" because AINOVA Test?
+  D.FULL_C <- subset( D.FULL, temp.n != "Unsicher" & D.FULL$migratory_beekeeper == "Nein" )
+  
+  CACHE.M <- F_EXTRACT_N( D.FULL_C, i[1], i[2] )
+  CACHE.BIND <- F_GLM_FACTOR( D.FULL_C, i[1], get( i[1], pos = D.FULL_C ) )
+  CACHE.BIND <- cbind( CACHE.M, CACHE.BIND )
+  D.FACTORS <- rbind( D.FACTORS, CACHE.BIND )
+}
+
+# Translate Factors into english for plotting
+D.FACTORS.PLOT <- D.FACTORS
+D.FACTORS.PLOT$ff[ D.FACTORS.PLOT$ff == "Ja" ] <- "Yes"
+D.FACTORS.PLOT$ff[ D.FACTORS.PLOT$ff == "Nein" ] <- "No"
+
+#### DISTRICTS Plot Matrix ####
+# Create DISTRICT DF & calculate loss rates
+D.DISTRICTS <- D.FULL %>% 
+  group_by( Bezirk, Bundesland ) %>% 
+  summarize( n = n(),
+             hives_lost = sum( hives_lost_e ) / sum( hives_winter ) * 100
+  )
+# We only use data when there are aleast 6n
+D.DISTRICTS <- subset( D.DISTRICTS, D.DISTRICTS$n > 5 )
+
+#### ADD DATA TO MAP_D #####
+MF_DISTRICTS$values = 0
+MF_DISTRICTS = left_join( MF_DISTRICTS, D.DISTRICTS, by = c( "id" = "Bezirk" ), copy = TRUE )
+
+#### MAPS ####
+D.PLOT_LIST <- list()
+D.CACHE <- list()
+
+for( i in oList){
+  D.FULL$ff <- get( i[1], pos = D.FULL ) 
+  D.CA <- subset( D.FULL, D.FULL$ff == "Ja", select = c( "latitude", "longitude" ) )
+  D.CACHE[[i[1]]] <- F_MAP_CLUSTER(D.CA)
+  # We need here aes_string!! inside functions or loops!
+  # https://www.rdocumentation.org/packages/ggplot2/versions/1.0.0/topics/aes_string
+  p_cache <-
+    ggplot() + 
+    geom_polygon(data = MF_DISTRICTS, aes( x = MF_DISTRICTS$long, y = MF_DISTRICTS$lat, group = MF_DISTRICTS$group, fill = MF_DISTRICTS$hives_lost ), show.legend = FALSE, color = "black", size = 0.2 ) + 
+    geom_path(data = MF_STATES, aes(x = MF_STATES$long, y = MF_STATES$lat, group = MF_STATES$group), color = "black", size = 0.6 ) + 
+    geom_point(data = D.CACHE[[i[1]]], aes_string(x = D.CACHE[[i[1]]]$longitude, y = D.CACHE[[i[1]]]$latitude, size = D.CACHE[[i[1]]]$n, colour = D.CACHE[[i[1]]]$n) ) + 
+    coord_fixed() +
+    xlab( "" ) + ylab( "" ) + labs( colour = "Reports (n)", size = "Reports (n)", fill = "" ) +
+    scale_fill_distiller( palette = "Greys", direction = 1, na.value = "white" ) +
+    # Info, when you want to join the legends for size and colour they need exact the same limits and breaks otherwise it wont work
+    scale_colour_distiller( breaks = c( 1, 5, 10, 15, 20 ), limits = c(0, 20), palette = "Reds", direction = 1, na.value = "white", guide = "legend" ) +
+    scale_size_continuous( range = c(0.1, 2), breaks = c( 1, 5, 10, 15, 20 ), limits = c(0, 20) ) + 
+    ggtitle(paste(i[2], "- rough location of yield")) +
+    theme_classic() +
+    theme(
+      legend.position = "bottom", 
+      legend.box = "horizontal",
+      plot.title = element_text(), 
+      axis.text = element_blank(), 
+      axis.ticks = element_blank(),
+      panel.grid.major = element_blank()
+    )
+  D.PLOT_LIST[[i[1]]] <- p_cache
+}
+
+
+
+#### PLOTTING #####
+p1 <- ggplot( data = D.FACTORS.PLOT ) +
+  aes( x = ff, y = middle ) + 
+  geom_bar( colour = "black", alpha = 0, fill = "white", show.legend = FALSE, stat = "identity", linetype = "longdash" ) + 
+  geom_pointrange( aes( ymin = lowerlim, ymax = upperlim ), size = 0.2 ) + 
+  geom_text( aes( x = ff, y = 0.5, label = paste("n = ", n )), angle = 0, vjust = 0, color = "black", size = 3 ) +
+  facet_wrap( ~ c, strip.position = "bottom", scales = "free_x", ncol = 3  ) +
+  xlab("") + ylab("Probability of loss [%]") + 
+  #ggtitle("Loss prob. by operational factors") +
+  theme_classic() + 
+  theme(
+    panel.spacing = unit( 1, "lines" ),
+    #strip.background = element_blank(),
+    strip.placement = "outside",
+    plot.title = element_text(hjust = 0.5), 
+    axis.title.x = element_text(colour = "black" ), 
+    axis.text.x = element_text(angle = 0, hjust = 0.5, size = 8, face = "bold"),
+    axis.line = element_line( linetype = "solid" ),
+    panel.grid.major.y = element_line( colour = "grey" ),
+    panel.grid.minor.y = element_line( colour = "grey" )
+    ) +
+  scale_x_discrete(
+    # labels = paste( D.FACTORS.PLOT$ff,"\n ( n = ",D.FACTORS.PLOT$n, " )", sep="" )
+  ) +
+  scale_y_continuous(
+    expand = c( 0 , 0 ),
+    breaks = seq( 0, 100, 5 )
+    #limits = c( 0, 20 )
+  )
+
+gtitle = textGrob( "Loss prob. by yield without migratory beekeepers" , gp=gpar( fontsize = 20 , face = "bold" ) )
+
+lay <- rbind( c( 1 ), c( 1 ), c( 2, 3 ), c( 4, 5 ), c( 6, 7 ))
+p1 <- arrangeGrob( p1, D.PLOT_LIST[[1]], D.PLOT_LIST[[2]], D.PLOT_LIST[[3]], D.PLOT_LIST[[4]], D.PLOT_LIST[[5]], D.PLOT_LIST[[6]],
+              top = gtitle, 
+              layout_matrix = lay)
+
+# Save File
+ggsave("./img/Plot_Factor_Yield.pdf", p1, width = 8, height = 12, units = "in")
+
