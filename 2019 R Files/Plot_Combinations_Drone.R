b577ef7c963836cfb525e6690697185b44c95786
Hannes Oberreiter <office@btree.at>
2019-08-17 19:25:12 +0200

    Drone Combination, added negative returns to get results for participants which did no drone brood removal

--- 2019 R Files/Plot_Combinations_Drone.R
+++ 2019 R Files/Plot_Combinations_Drone.R
@@ -39,18 +39,28 @@ ColComb3 <- combn( ColComb1 , 3, simplify = FALSE )
 CacheList <- data.frame(t(sapply(oList, c)))
 CacheList <- cbind(CacheList, ColComb1)
 
-CACHE.COMB <- F_COMBINATION(D.FULL, ColComb1, 1, CacheList, ColComb1)
+# negative logic, participants which did not 
+CACHE.COMB.negative <- F_COMBINATION(D.FULL, ColComb1, 1, CacheList, ColComb1, 0)[1:2,]
+CACHE.COMB.negative$ff <- "No"
+CACHE.COMB1.negative <- F_COMBINATION(D.FULL, ColComb2, 2, CacheList, ColComb1,0)[1,]
+CACHE.COMB1.negative$ff <- "No"
+
+# positive logic, participants which did
+CACHE.COMB <- F_COMBINATION(D.FULL, ColComb1, 1, CacheList, ColComb1, 1)
+CACHE.COMB$ff <- "Yes"
 CACHE.COMB1 <- F_COMBINATION(D.FULL, ColComb2, 2, CacheList, ColComb1)
-CACHE.COMB <- rbind(CACHE.COMB, CACHE.COMB1)
-CACHE.COMB1 <- F_COMBINATION(D.FULL, ColComb3, 3, CacheList, ColComb1)
-CACHE.COMB <- rbind(CACHE.COMB, CACHE.COMB1)
+CACHE.COMB1$ff <- "Yes"
+
+CACHE.COMB.PLOT <- rbind(CACHE.COMB, CACHE.COMB1, CACHE.COMB1.negative)
+#CACHE.COMB1 <- F_COMBINATION(D.FULL, ColComb3, 3, CacheList, ColComb1)
+#CACHE.COMB <- rbind(CACHE.COMB, CACHE.COMB1)
 
 # D.COMB <- D.COMB[D.COMB$n != 0, ]
 
-xAxisTemp <- c("Only in spring", "Only in summer", "Spring and summer")
+xAxisTemp <- c("Only in spring", "Only in summer", "Spring and summer", "No drone brood removal")
 
 p1 <- 
-  ggplot( CACHE.COMB, aes( x = xAxisTemp, y = middle )) +
+  ggplot( CACHE.COMB.PLOT, aes( x = xAxisTemp, y = middle )) +
   geom_bar( colour = "black", alpha = 0, fill = "white", show.legend = FALSE, stat = "identity", linetype = "longdash" ) + 
   geom_pointrange( aes( ymin = lowerlim, ymax = upperlim ), size = 1.0 ) + 
   geom_text( aes( x = xAxisTemp, y = 0.5, label = paste("n = ", n )), angle = 0, vjust = 0, color = "black", size = 3 ) +
@@ -73,4 +83,4 @@ p1 <-
     breaks = seq( 0, 100, 5 )
   )
 
-ggsave("./img/Plot_Drone_Removal.pdf", p1, width = 4, height = 3, units = "in")
+ggsave("./img/Plot_Drone_Removal.pdf", p1, width = 6, height = 3, units = "in")
