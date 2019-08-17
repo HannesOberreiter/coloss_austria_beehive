272ecc789c6f79e35eaea264b09fd82602b5be5f
Hannes Oberreiter <office@btree.at>
2019-08-17 19:25:51 +0200

    Operational Plot, added automatic significant star from anova chisqr result and changes some names

--- 2019 R Files/Plot_Factor_Operational.R
+++ 2019 R Files/Plot_Factor_Operational.R
@@ -21,15 +21,15 @@ source( "Partials_Functions.r" )
 
 # List of Factors we want in our Plot
 oList = list(
-  c("no_foundation", "(H) No foundation"),
-  c("foreign_wax", "(G) Foreign wax"),
+  c("no_foundation", "(H) Natural comb (without foundation)"),
+  c("foreign_wax", "(G) Purchase wax from outside own operation"),
   c("migratory_beekeeper", "(B) Migration"),
-  c("mash_bottom_board", "(F) Mash bottom"),
+  c("mash_bottom_board", "(F) Screened (mesh) bottom board in Winter"),
   c("insulated_hives", "(E) Insulated hives"),
-  c("plastic_hives", "(D) Plastic hives"),
-  c("cert_org_beek", "(A) Cert. organic"),
-  c("varroatolerant", "(C) VSH bees"),
-  c("small_broodcells", "(I) Small broodcells")
+  c("plastic_hives", "(D) Hives made from synthetic materials"),
+  c("cert_org_beek", "(A) Certified organic beekeeping"),
+  c("varroatolerant", "(C) Queens bred from Varroa tolerant/resistant stock"),
+  c("small_broodcells", "(I) Small brood cell size (5.1 mm or less)")
 )
 
 # Create dummy Dataframe, to insert rows later
@@ -43,7 +43,7 @@ D.FACTORS <-
 for( i in oList){
   D.FULL_C <- D.FULL
   CACHE.M <- F_EXTRACT_N( D.FULL_C, i[1], i[2] )
-  CACHE.BIND <- F_GLM_FACTOR( D.FULL_C, i[1], get( i[1], pos = D.FULL_C ) )
+  CACHE.BIND <- F_GLM_FACTOR( D.FULL_C, i[1], get( i[1], pos = D.FULL_C ), TRUE )
   CACHE.BIND <- cbind( CACHE.M, CACHE.BIND )
   D.FACTORS <- rbind( D.FACTORS, CACHE.BIND )
 }
@@ -65,7 +65,8 @@ p1 <- ggplot( data = D.FACTORS.PLOT ) +
   geom_bar( colour = "black", alpha = 0, fill = "white", show.legend = FALSE, stat = "identity", linetype = "longdash" ) + 
   geom_pointrange( aes( ymin = lowerlim, ymax = upperlim ), size = 0.2 ) + 
   geom_text( aes( x = ff, y = 0.5, label = paste("n = ", n )), angle = 0, vjust = 0, color = "black", size = 2.5 ) +
-  facet_wrap( ~ c, strip.position = "bottom", scales = "free_x", ncol = 5  ) +
+  geom_text(data =  D.FACTORS.PLOT[(D.FACTORS.PLOT$chistar == 1 & D.FACTORS.PLOT$ff == 'Yes'),], aes( x = ff, y = 20, label = "*"), angle = 0, vjust = 0, hjust = -4, color = "black", size = 8 ) +
+  facet_wrap( ~ c, strip.position = "bottom", scales = "free_x", ncol = 5, labeller = label_wrap_gen(width=30)  ) +
   xlab("") + ylab("Loss rate [%]") + 
   theme_classic() + 
   theme(
