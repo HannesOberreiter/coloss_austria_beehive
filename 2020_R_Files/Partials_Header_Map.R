1e851492976e6e7d2b92fdc21cda25477f7b08db
Hannes Oberreiter <office@btree.at>
2020-05-01 15:54:09 +0200

    removed unsused libs

--- 2020_R_Files/Partials_Header_Map.R
+++ 2020_R_Files/Partials_Header_Map.R
@@ -4,16 +4,8 @@
 # Header File, if you use a map in your plot
 
 # ---- MAP Libraries ----
-library(rgeos)
-library(stringr)
-library(rgdal)
-library(maptools)
-library(maps)
-#library(mapproj)
-# Error Workaround
-library(gpclib)
-#if (!require(gpclib)) install.packages("gpclib", type="source")
-gpclibPermit()
+library( rgdal )
+
 
 SCRIPT.DIR <- dirname( rstudioapi::getActiveDocumentContext()$path )
 setwd( SCRIPT.DIR )
@@ -26,7 +18,7 @@ SHAPEFILE.NAME.AUSTRIA <- "VGD_modf"
 SHAPEFILE.FOLDER.AUSTRIA  <- gsub(" ", "", paste(SCRIPT.DIR, SHAPEFILE.NAME.AUSTRIA, sep = "/"), fixed = TRUE)
 
 # ---- Read MAPS -----
-MAP_AUSTRIA    <- readOGR( dsn = SHAPEFILE.FOLDER.AUSTRIA, layer = SHAPEFILE.NAME.AUSTRIA )
+MAP_AUSTRIA   <- readOGR( dsn = SHAPEFILE.FOLDER.AUSTRIA, layer = SHAPEFILE.NAME.AUSTRIA )
 MF_STATES     <- fortify( MAP_AUSTRIA, region = "BL" )
 MF_DISTRICTS  <- fortify( MAP_AUSTRIA, region = "PB" )
 
