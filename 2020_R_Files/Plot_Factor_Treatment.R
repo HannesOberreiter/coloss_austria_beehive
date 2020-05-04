ba9f0ea519b6f3d514c878c8d645da73c7282840
Hannes Oberreiter <office@btree.at>
2020-05-04 22:10:47 +0200

    treatment factors updated for 2020

--- 2020_R_Files/Plot_Factor_Treatment.R
+++ 2020_R_Files/Plot_Factor_Treatment.R
@@ -1,11 +1,11 @@
 ##############################
 ##############################
 ### Survey Bee Hive Losses ###
-# (c) 2019 Hannes Oberreiter #
+# (c) 2020 Hannes Oberreiter #
 ##############################
 ##############################
 
-####### OPTERATION FACTOR PLOT ###########
+####### ALL SINGLE TREATMENT PLOTS ###########
 
 # Set Working directory (uses API of RStudio)
 SCRIPT.DIR <- dirname( rstudioapi::getActiveDocumentContext()$path )
@@ -19,263 +19,114 @@ source( "Partials_Header_Treatment.r" )
 source( "Partials_Functions.r" )
 
 #### START CODE #####
+D.FULL <- D.RAW
+# Remove participants which did not answer varroa_treated (this includes eg. beekeeping journal without given question)
+D.FULL <- D.FULL[!is.na(D.FULL$varroa_treated)]
+# Only select people which did treatment and give us at least some months
+D.FULL <- D.FULL[D.FULL$varroa_treated == "Ja" & D.FULL$T_amount > 0]
 
 # Just test for queen related mortality
 #D.FULL$hives_lost_e <- D.FULL$lost_a
 #D.FULL$hives_spring_e <- D.FULL$hives_spring_queen
 
-#### SPRING ####
+V.SEASONS <- c("spring", "summer", "winter")
+V.COLORS <- c("cornflowerblue", "forestgreen", "azure3")
 
-# Create dummy Dataframe, to insert rows later
-D.FACTORS <- 
-  setNames( 
-    data.frame( matrix( ncol = 11, nrow = 0)), 
-    c( "ff", "c", "n", "hives_winter", "lost_a", "lost_b", "lost_c", "hives_lost_rate", "lowerlim", "middle", "upperlim")
+# temporary list
+D.FACTORS = list()
+
+D.FULL$T_drone_totalyn_spring
+
+for( i in treatmentList[-1]){
+  for(j in V.SEASONS){
+    V.REGEX <- paste( i[2], "yn_", j, sep = "")
+    # Check if there is only "no", means this treatment was not used at this time, next jumps to next iternation
+    if( length( unique( get( V.REGEX, pos = D.FULL ))) == 1) next
+    CACHE.M <- F_EXTRACT_N( D.FULL, V.REGEX, i[3] )
+    # If there are not over 19 n we skip it
+    if( (CACHE.M$n[CACHE.M$ff == "1"]) < 20) next
+    # add season column to later split it
+    CACHE.M$season <- j  
+    CACHE.BIND <- F_GLM_FACTOR( D.FULL, V.REGEX, get( V.REGEX, pos = D.FULL ), TRUE )
+    D.FACTORS[[paste(i[3], j, sep="-")]] <- cbind( CACHE.M, CACHE.BIND )
+  }
+}
+# Change List to tibble
+D.FACTORS <- bind_rows(D.FACTORS)
+# cleanup
+rm(i,j,V.REGEX, CACHE.M, CACHE.BIND)
+# Rename 1 and 0
+D.FACTORS$ff <- ifelse(D.FACTORS$ff == 1, "Ja", "Nein")
+# Ordering
+D.FACTORS$ff <- factor( D.FACTORS$ff, levels = c( "Ja", "Nein"))
+# Chistar Label
+D.FACTORS$chistar_label <- ifelse(D.FACTORS$chistar == 1, "*", "")
+
+# Creating xAxisLetters and add it to DF
+V.LETTERS <- c(LETTERS[1:26], paste0("A",LETTERS[1:26]))
+D.FACTORS$letter <- ""
+for(i in V.SEASONS){
+  # logical vector for subsetting the different seasons in the dataframe
+  V.SUB <- D.FACTORS$season == i
+  # generate labels
+  V.LENGTH <- length(unique(D.FACTORS$c[V.SUB]))
+  V.LETTER_TEMP <- V.LETTERS[1:V.LENGTH]
+  V.LETTER_TEMP <- c(V.LETTER_TEMP, V.LETTER_TEMP)
+  V.LETTER_TEMP <- sort(V.LETTER_TEMP)
+  # add labels to dataframe with name
+  D.FACTORS$letter[V.SUB] <- paste("(",V.LETTER_TEMP,") ", D.FACTORS$c[V.SUB], sep = "")
+}
+# cleanup
+rm(V.SUB, V.LENGTH, V.LETTER_TEMP, V.LETTERS, i)
+
+L.PLOTS <- list()
+for (i in 1:length(V.SEASONS)) {
+  V.season = V.SEASONS[i]
+  V.color <- V.COLORS[i]
+  
+  # generate chistar brackets temporary dataframe
+  D.TEMP <- D.FACTORS[D.FACTORS$season == V.season & D.FACTORS$chistar == 1,]
+  D.ANNOTATION <- 
+    data.frame(
+      letter = D.TEMP$letter[D.TEMP$ff == "Ja"], 
+      start  = D.TEMP$ff[D.TEMP$ff == "Ja"], 
+      end    = D.TEMP$ff[D.TEMP$ff == "Nein"], 
+      y      = ifelse(
+        (D.TEMP$upperlim[D.TEMP$ff == "Ja"] > D.TEMP$upperlim[D.TEMP$ff == "Nein"]), 
+        D.TEMP$upperlim[D.TEMP$ff == "Ja"],
+        D.TEMP$upperlim[D.TEMP$ff == "Nein"]) + 5, 
+      label  = D.TEMP$chistar_label[D.TEMP$ff == "Ja"]
     )
-
-
-# Remove people which did not answer or did no treatment
-D.FULL <- D.FULL[(D.FULL$T_amount_total > 0 | (D.FULL$varroa_treated == "Nein" & !is.na(D.FULL$varroa_treated))), ]
-
-# Loop through list and create for all factors CI
-for( i in treatmentList){
-  xn <- paste( i[2], "yn_spring", sep = "")
-  # Check if there is only "no", means this treatment was not used at this time, next jumps to next iternation
-  if( length( unique( get( xn, pos = D.FULL ))) == 1) next
-  CACHE.M <- F_EXTRACT_N( D.FULL, xn, i[3] )
   
-  # If there are not over 19 n we skip it
-  testN <- CACHE.M$n[CACHE.M$ff == "1"]
-  if( testN < 20) next
-  
-  CACHE.BIND <- F_GLM_FACTOR( D.FULL, xn, get( xn, pos = D.FULL ), TRUE )
-  CACHE.BIND <- cbind( CACHE.M, CACHE.BIND )
-  D.FACTORS <- rbind( D.FACTORS, CACHE.BIND )
-}
-
-# Rename 1 and 0
-D.FACTORS$ff <- ifelse(D.FACTORS$ff == 0, "No", "Yes")
-# Ordering
-D.FACTORS$ff <- factor( D.FACTORS$ff, 
-                             levels = c( "Yes", "No", "Uncertain"))
-
-# Creating xAxisLetters and add it to DF
-xAxisMaxLength <- length(unique(D.FACTORS$c))
-# Small function to generate more than 26 letters eg Z AA AB 
-xAxisTemp <- c(LETTERS[1:26], paste0("A",LETTERS[1:26]))
-xAxisTemp <- xAxisTemp[1:xAxisMaxLength]
-xAxisTemp <- c(xAxisTemp, xAxisTemp)
-xAxisTemp <- sort(xAxisTemp)
-D.FACTORS$letter <- paste("(",xAxisTemp,") ", D.FACTORS$c, sep = "")
-
-D.FACTORS.SPRING <- D.FACTORS
-
-p1 <- ggplot( data = D.FACTORS ) +
-  aes( x = ff, y = middle ) + 
-  geom_crossbar(aes( ymin = lowerlim, ymax = upperlim ), fill = "cornflowerblue") +
-  geom_point(size = 3) + 
-  #geom_bar( colour = "black", alpha = 0.3, fill = "cornflowerblue", show.legend = FALSE, stat = "identity", linetype = "longdash" ) + 
-  #geom_pointrange( aes( ymin = lowerlim, ymax = upperlim ), size = 0.2 ) + 
-  geom_text( aes( x = ff, y = 0.5, label = paste("n = ", n )), angle = 0, vjust = 0, color = "black", size = 2.5 ) +
-  geom_text(data = D.FACTORS[(D.FACTORS$chistar == 1 & D.FACTORS$ff == 'Yes'),], aes( x = ff, y = 20, label = "*"), angle = 0, vjust = 0, hjust = -5, color = "black", size = 8 ) +
-  facet_wrap( ~ letter, strip.position = "top", scales = "free_x", ncol = 5  ) +
-  xlab("") + ylab("Loss rate [%]") + 
-  theme_classic() + 
-  theme(
-    panel.spacing = unit( 1, "lines" ),
-    strip.placement = "outside",
-    plot.title = element_text(hjust = 0.5), 
-    axis.title.x = element_text(colour = "black" ), 
-    axis.text.x = element_text(angle = 0, hjust = 0.5, size = 9, face = "bold"),
-    axis.line = element_line( linetype = "solid", size = 0.5 ),
-    axis.text.y = element_text(angle = 0, size = 10),
-    panel.grid.major.y = element_line( colour = "grey" ),
-    panel.grid.minor.y = element_line( colour = "grey" )
+  L.PLOTS[[V.season]] <- ggplot( data = D.FACTORS[D.FACTORS$season == V.season,] ) +
+    aes( x = ff, y = middle ) + 
+    geom_crossbar(aes( ymin = lowerlim, ymax = upperlim ), fill = V.color) +
+    geom_point(size = 3) + 
+    geom_text(aes( x = ff, y = 0.5, label = paste("n = ", n )), angle = 0, vjust = 0, color = "black", size = 2.5 ) +
+    geom_signif(data=D.ANNOTATION, aes(xmin=start, xmax=end, annotations=label, y_position=y), textsize = 8, manual=TRUE) +
+    facet_wrap( ~ letter, strip.position = "top", scales = "free_x", ncol = 5  ) +
+    xlab("") + ylab("Verlustrate [%]") + 
+    theme_classic() + 
+    theme(
+      panel.spacing = unit( 1, "lines" ),
+      strip.placement = "outside",
+      plot.title = element_text(hjust = 0.5), 
+      axis.title.x = element_text(colour = "black" ), 
+      axis.text.x = element_text(angle = 0, hjust = 0.5, size = 9, face = "bold"),
+      axis.line = element_line( linetype = "solid", size = 0.5 ),
+      axis.text.y = element_text(angle = 0, size = 10),
+      panel.grid.major.y = element_line( colour = "grey" ),
+      panel.grid.minor.y = element_line( colour = "grey" )
     ) +
-  scale_x_discrete(
-  ) +
-  scale_y_continuous(
-    expand = c( 0 , 0 ),
-    breaks = seq( 0, 100, 5 ),
-    limits = c( 0, 30 )
-  )
-
-#gtitle = textGrob( "Loss rate by treatment method - SPRING (April, May)" , gp=gpar( fontsize = 16 , face = "bold" ) )
-
-lay <- rbind( c( 1 ) )
-p <- arrangeGrob( p1,
-              #top = gtitle, 
-              layout_matrix = lay)
-
-ggsave("./img/P_FACTOR_Treatment_Spring.pdf", p, width = 12, height = 3, units = "in")
-
-
-#### SUMMER ####
-
-# Create dummy Dataframe, to insert rows later
-D.FACTORS <- 
-  setNames( 
-    data.frame( matrix( ncol = 11, nrow = 0)), 
-    c( "ff", "c", "n", "hives_winter", "lost_a", "lost_b", "lost_c", "hives_lost_rate", "lowerlim", "middle", "upperlim")
-  )
-
-# Loop through list and create for all factors CI
-for( i in treatmentList){
-  xn <- paste( i[2], "yn_summer", sep = "")
-  # Check if there is only "no", means this treatment was not used at this time, next jumps to next iternation
-  if( length( unique( get( xn, pos = D.FULL ))) == 1) next
-  CACHE.M <- F_EXTRACT_N( D.FULL, xn, i[3] )
-  
-  # If there are not over 19 n we skip it
-  testN <- CACHE.M$n[CACHE.M$ff == "1"]
-  if( testN < 20) next
-  
-  CACHE.BIND <- F_GLM_FACTOR( D.FULL, xn, get( xn, pos = D.FULL ), TRUE )
-  CACHE.BIND <- cbind( CACHE.M, CACHE.BIND )
-  D.FACTORS <- rbind( D.FACTORS, CACHE.BIND )
+    scale_x_discrete(
+    ) +
+    scale_y_continuous(
+      expand = c( 0 , 0 ),
+      breaks = seq( 0, 100, 5 ),
+      limits = c( 0, 30 )
+    )
 }
 
-# Rename 1 and 0
-D.FACTORS$ff <- ifelse(D.FACTORS$ff == 0, "No", "Yes")
-# Ordering
-D.FACTORS$ff <- factor( D.FACTORS$ff, 
-                        levels = c( "Yes", "No", "Uncertain"))
-
-# Creating xAxisLetters and add it to DF
-xAxisMaxLength <- length(unique(D.FACTORS$c))
-# Small function to generate more than 26 letters eg Z AA AB 
-xAxisTemp <- c(LETTERS[1:26], paste0("A",LETTERS[1:26]))
-xAxisTemp <- xAxisTemp[1:xAxisMaxLength]
-xAxisTemp <- c(xAxisTemp, xAxisTemp)
-xAxisTemp <- sort(xAxisTemp)
-D.FACTORS$letter <- paste("(",xAxisTemp,") ", D.FACTORS$c, sep = "")
-
-D.FACTORS.SUMMER <- D.FACTORS
-
-p1 <- ggplot( data = D.FACTORS ) +
-  aes( x = ff, y = middle ) + 
-  geom_crossbar(aes( ymin = lowerlim, ymax = upperlim ), fill = "forestgreen") +
-  #geom_bar( colour = "black", alpha = 0.3, fill = "forestgreen", show.legend = FALSE, stat = "identity", linetype = "longdash" ) + 
-  #geom_pointrange( aes( ymin = lowerlim, ymax = upperlim ), size = 0.2 ) + 
-  geom_point(size = 3) + 
-  geom_text( aes( x = ff, y = 0.5, label = paste("n = ", n )), angle = 0, vjust = 0, color = "black", size = 2.5 ) +
-  geom_text(data = D.FACTORS[(D.FACTORS$chistar == 1 & D.FACTORS$ff == 'Yes'),], aes( x = ff, y = 20, label = "*"), angle = 0, vjust = 0, hjust = -5, color = "black", size = 8 ) +
-  facet_wrap( ~ letter, strip.position = "top", scales = "free_x", ncol = 4  ) +
-  xlab("") + ylab("Loss rate [%]") + 
-  theme_classic() + 
-  theme(
-    panel.spacing = unit( 1, "lines" ),
-    strip.placement = "outside",
-    plot.title = element_text(hjust = 0.5), 
-    axis.title.x = element_text(colour = "black" ), 
-    axis.text.x = element_text(angle = 0, hjust = 0.5, size = 9, face = "bold"),
-    axis.line = element_line( linetype = "solid", size = 0.5 ),
-    axis.text.y = element_text(angle = 0, size = 10),
-    panel.grid.major.y = element_line( colour = "grey" ),
-    panel.grid.minor.y = element_line( colour = "grey" )
-  ) +
-  scale_x_discrete(
-  ) +
-  scale_y_continuous(
-    expand = c( 0 , 0 ),
-    breaks = seq( 0, 100, 5 ),
-    limits = c( 0, 31 )
-  )
-
-#gtitle = textGrob( "Loss rate by treatment method - SUMMER (June - October)" , gp=gpar( fontsize = 16 , face = "bold" ) )
-
-lay <- rbind( c( 1 ) )
-p <- arrangeGrob( p1,
-                   #top = gtitle, 
-                   layout_matrix = lay)
-
-ggsave("./img/P_FACTOR_Treatment_Summer.pdf", p, width = 12, height = 8, units = "in")
-
-
-
-#### WINTER ####
-
-# Create dummy Dataframe, to insert rows later
-D.FACTORS <- 
-  setNames( 
-    data.frame( matrix( ncol = 11, nrow = 0)), 
-    c( "ff", "c", "n", "hives_winter", "lost_a", "lost_b", "lost_c", "hives_lost_rate", "lowerlim", "middle", "upperlim")
-  )
-
-# Loop through list and create for all factors CI
-for( i in treatmentList){
-  xn <- paste( i[2], "yn_winter", sep = "")
-  # Check if there is only "no", means this treatment was not used at this time, next jumps to next iternation
-  if( length( unique( get( xn, pos = D.FULL ))) == 1) next
-  CACHE.M <- F_EXTRACT_N( D.FULL, xn, i[3] )
-  
-  # If there are not over 19 n we skip it
-  testN <- CACHE.M$n[CACHE.M$ff == "1"]
-  if( testN < 20) next
-  
-  CACHE.BIND <- F_GLM_FACTOR( D.FULL, xn, get( xn, pos = D.FULL ), TRUE )
-  CACHE.BIND <- cbind( CACHE.M, CACHE.BIND )
-  D.FACTORS <- rbind( D.FACTORS, CACHE.BIND )
-}
-
-# Rename 1 and 0
-D.FACTORS$ff <- ifelse(D.FACTORS$ff == 0, "No", "Yes")
-# Ordering
-D.FACTORS$ff <- factor( D.FACTORS$ff, 
-                        levels = c( "Yes", "No", "Uncertain"))
-
-# Creating xAxisLetters and add it to DF
-xAxisMaxLength <- length(unique(D.FACTORS$c))
-# Small function to generate more than 26 letters eg Z AA AB 
-xAxisTemp <- c(LETTERS[1:26], paste0("A",LETTERS[1:26]))
-xAxisTemp <- xAxisTemp[1:xAxisMaxLength]
-xAxisTemp <- c(xAxisTemp, xAxisTemp)
-xAxisTemp <- sort(xAxisTemp)
-D.FACTORS$letter <- paste("(",xAxisTemp,") ", D.FACTORS$c, sep = "")
-
-D.FACTORS.WINTER <- D.FACTORS
-
-D.FACTORS[(D.FACTORS$chistar == 1 & D.FACTORS$ff == 'Yes'),]
-
-p1 <- ggplot( data = D.FACTORS ) +
-  aes( x = ff, y = middle ) + 
-  geom_crossbar(aes( ymin = lowerlim, ymax = upperlim ), fill = "grey13", alpha = 0.3) +
-  geom_point(size = 3) + 
-  #geom_bar( colour = "black", alpha = 0.3, fill = "grey13", show.legend = FALSE, stat = "identity", linetype = "longdash" ) + 
-  #geom_pointrange( aes( ymin = lowerlim, ymax = upperlim ), size = 0.2 ) + 
-  geom_text( aes( x = ff, y = 0.5, label = paste("n = ", n )), angle = 0, vjust = 0, color = "black", size = 2.5 ) +
-  
-  ## commented out because we dont have a significant row TODO change with If?
-  
-  #geom_text(data = D.FACTORS[(D.FACTORS$chistar == 1 & D.FACTORS$ff == 'Yes'),], aes( x = ff, y = 20, label = "*"), angle = 0, vjust = 0, hjust = -5, color = "black", size = 8 ) +
-  facet_wrap( ~ letter, strip.position = "top", scales = "free_x", ncol = 4  ) +
-  xlab("") + ylab("Loss rate [%]") + 
-  theme_classic() + 
-  theme(
-    panel.spacing = unit( 1, "lines" ),
-    strip.placement = "outside",
-    plot.title = element_text(hjust = 0.5), 
-    axis.title.x = element_text(colour = "black" ), 
-    axis.text.x = element_text(angle = 0, hjust = 0.5, size = 9, face = "bold"),
-    axis.line = element_line( linetype = "solid", size = 0.5 ),
-    axis.text.y = element_text(angle = 0, size = 10),
-    panel.grid.major.y = element_line( colour = "grey" ),
-    panel.grid.minor.y = element_line( colour = "grey" )
-  ) +
-  scale_x_discrete(
-  ) +
-  scale_y_continuous(
-    expand = c( 0 , 0 ),
-    breaks = seq( 0, 100, 5 ),
-    limits = c( 0, 30 )
-  )
-
-#gtitle = textGrob( "Loss rate by treatment method - WINTER (November - January)" , gp=gpar( fontsize = 16 , face = "bold" ) )
-
-lay <- rbind( c( 1 ) )
-p <- arrangeGrob( p1,
-                   #top = gtitle, 
-                   layout_matrix = lay)
-
-ggsave("./img/P_FACTOR_Treatment_Winter.pdf", p, width = 12, height = 3, units = "in")
-
+ggsave("./img/plot_treatment_spring.pdf", L.PLOTS[[1]], width = 12, height = 8, units = "in")
+ggsave("./img/plot_treatment_summer.pdf", L.PLOTS[[2]], width = 12, height = 8, units = "in")
+ggsave("./img/plot_treatment_winter.pdf", L.PLOTS[[3]], width = 12, height = 8, units = "in")
