a951a4de9c5a9de4fc0f5f0acf4fd13346c32475
Hannes Oberreiter <office@btree.at>
2020-05-07 17:39:23 +0200

    bugfix in map header when loading the maps in wrong order, also only load maps if needed

--- 2020_R_Files/Partials_Header_Map.R
+++ 2020_R_Files/Partials_Header_Map.R
@@ -4,8 +4,10 @@
 # Header File, if you use a map in your plot
 
 # ---- MAP Libraries ----
-library( rgdal )
+# Important loading order to prevent errors! (https://stackoverflow.com/questions/30790036/error-istruegpclibpermitstatus-is-not-true)
+library( rgeos )
 library( maptools )
+library( rgdal )
 
 SCRIPT.DIR <- dirname( rstudioapi::getActiveDocumentContext()$path )
 setwd( SCRIPT.DIR )
@@ -17,9 +19,11 @@ SHAPEFILE.NAME.AUSTRIA <- "VGD_modf"
 SHAPEFILE.FOLDER.AUSTRIA  <- gsub(" ", "", paste(SCRIPT.DIR, SHAPEFILE.NAME.AUSTRIA, sep = "/"), fixed = TRUE)
 
 # ---- Read MAPS -----
-MAP_AUSTRIA   <- readOGR( dsn = SHAPEFILE.FOLDER.AUSTRIA, layer = SHAPEFILE.NAME.AUSTRIA )
-MF_STATES     <- fortify( MAP_AUSTRIA, region = "BL" )
-MF_DISTRICTS  <- fortify( MAP_AUSTRIA, region = "PB" )
+if(!exists("MAP_AUSTRIA")){
+  MAP_AUSTRIA   <- readOGR( dsn = SHAPEFILE.FOLDER.AUSTRIA, layer = SHAPEFILE.NAME.AUSTRIA )
+  MF_STATES     <- fortify( MAP_AUSTRIA, region = "BL" )
+  MF_DISTRICTS  <- fortify( MAP_AUSTRIA, region = "PB" )
+}
 
 # ---- Remove Temporary -----
 rm(SHAPEFILE.FOLDER.AUSTRIA, SHAPEFILE.NAME.AUSTRIA)
