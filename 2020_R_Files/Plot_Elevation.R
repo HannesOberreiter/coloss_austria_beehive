e813959fb8bfd52907e4eac477eb343a0d8f4cba
Hannes Oberreiter <office@btree.at>
2020-05-25 12:19:16 +0200

    cleanup 2020

--- 2020_R_Files/Plot_Elevation.R
+++ 2020_R_Files/Plot_Elevation.R
@@ -1,11 +1,11 @@
 ##############################
 ##############################
 ### Survey Bee Hive Losses ###
-# (c) 2019 Hannes Oberreiter #
+# (c) 2020 Hannes Oberreiter #
 ##############################
 ##############################
 
-####### Altitude LOSSES PLOT ###########
+####### Elevation LOSSES PLOT ###########
 
 # Set Working directory (uses API of RStudio)
 SCRIPT.DIR <- dirname( rstudioapi::getActiveDocumentContext()$path )
@@ -16,53 +16,24 @@ source( "Partials_Header.r" )
 # Import our Custom Functions
 source( "Partials_Functions.r" )
 
-D.CACHE <- D.FULL
+#### Start Code ####
+D.FULL <- D.RAW
+V.SEQ <- c(seq( 0, 800, 200 ), Inf)
+V.GROUPS <- c( "0-200m", "201-400m", "401-600m", "601-800m", ">800m" )
+D.FULL$altitude_group <- cut(
+  D.FULL$altitude, 
+  V.SEQ, label = V.GROUPS,
+  include.lowest = TRUE, right = TRUE )
 
-L.SEQ <- c(seq( 0, 800, 200 ), Inf)
-L.GROUPS <- c( "0-200m", "201-400m", "401-600m", "601-800m", ">800m" )
-D.CACHE$altitude_group <- cut( D.CACHE$Altitude, L.SEQ, label = L.GROUPS, include.lowest = TRUE, right = TRUE )
+D.FULL <- D.FULL[!is.na(D.FULL$altitude_group),]
+D.FACTORS <- F_EXTRACT_N(D.FULL, "altitude_group", "altitude_group", TRUE)
+CACHE.BIND <- F_GLM_FACTOR( D.FULL, "altitude_group", D.FULL$altitude_group, TRUE)
+D.FACTORS <- cbind( D.FACTORS, CACHE.BIND )
 
-D.PLOT_Q <- D.CACHE %>%
-  group_by(altitude_group) %>%
-  summarise(
-    n = n(),
-    np = F_NUMBER_FORMAT(n() / nrow(D.CACHE) * 100)
-  )
-
-
-CACHE.BIND <- F_GLM_FACTOR( D.CACHE, "altitude_group", D.CACHE$altitude_group )
-D.PLOT_Q <- cbind( D.PLOT_Q, CACHE.BIND )
-
-p <- ggplot( data = D.PLOT_Q ) +
-  aes( x = altitude_group, y = middle ) + 
-  geom_crossbar(aes( ymin = lowerlim, ymax = upperlim ), fill = "white") +
-  #geom_bar( colour = "black", alpha = 0, fill = "white", show.legend = FALSE, stat = "identity", linetype = "longdash" ) + 
-  #geom_pointrange( aes( ymin = lowerlim, ymax = upperlim ), size = 0.2 ) + 
-  geom_point(size = 3) + 
-  geom_text( aes( x = altitude_group, y = 0.5, label = paste("n = ", n )), color = "black", size = 2.5 ) +
-  xlab("Elevation [m above sea level]") + ylab("Loss rate [%]") + 
-  #ggtitle("Loss rate to elevation of main wintering apiary") +
-  theme_classic() + 
-  theme(
-    panel.spacing = unit( 1, "lines" ),
-    strip.placement = "outside",
-    plot.title = element_text(), 
-    axis.title.x = element_text(colour = "black" ), 
-    axis.text.x = element_text(angle = 0, size = 8, face = "bold"),
-    axis.line = element_line( linetype = "solid" ),
-    panel.grid.major.y = element_line( colour = "grey" )
-  ) +
-  scale_x_discrete(
-  ) +
-  scale_y_continuous(
-    limits = c(0, NA),
-    expand = c( 0 , 0 ),
-    breaks = seq( 0, 100, 5 )
-  )
+# cleanup
+rm(CACHE.BIND)
 
 
-# Save File
-ggsave("./img/Plot_Elevation.pdf", p, width = 6, height = 3, units = "in")
-
-
+p <- F_SINGLE_PLOT(D.FACTORS)
+ggsave("./img/plot_elevation.pdf", p, width = 5, height = 4, units = "in")
 
