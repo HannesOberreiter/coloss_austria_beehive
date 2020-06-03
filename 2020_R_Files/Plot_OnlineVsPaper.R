b3aa51fd339ab6b517fc0c7193004b6b66bacd4d
Hannes Oberreiter <office@btree.at>
2020-06-03 21:55:24 +0200

    added sign. bar

--- 2020_R_Files/Plot_OnlineVsPaper.R
+++ 2020_R_Files/Plot_OnlineVsPaper.R
@@ -19,17 +19,20 @@ source( "Partials_Functions.r" )
 
 #### START CODE #####
 D.FULL <- D.RAW
+D.FULL$submitted[D.FULL$submitted == "Zeitung"] <- "Papier"
 
-CACHE.M     <- F_EXTRACT_N( D.FULL, "submitted", "Online vs. Paper vs. Zeitung" )
+CACHE.M     <- F_EXTRACT_N( D.FULL, "submitted", "Online vs. Papier" )
 CACHE.BIND  <- F_GLM_FACTOR( D.FULL, "submitted", D.FULL$submitted, TRUE )
 D.FACTORS  <- cbind( CACHE.M, CACHE.BIND )
 
 # cleanup
 rm(CACHE.M, CACHE.BIND)
 
-D.FACTORS.PLOT <- D.FACTORS
-
-p <- F_SINGLE_PLOT(D.FACTORS.PLOT)
-
-ggsave("./img/plot_onlinevspaper.pdf", p, width = 5, height = 4, units = "in")
+p <- F_SINGLE_PLOT(D.FACTORS)
+D.ANNOTATION <- F_CHISTAR_DF(D.FACTORS, "Internet", "Papier", "c")
+if(nrow(D.ANNOTATION)> 0){
+  p1 <- p + geom_signif(data=D.ANNOTATION, aes(xmin=start, xmax=end, annotations=label, y_position=y), textsize = 8, manual=TRUE)
+}
+p1
+ggsave("./img/plot_onlinevspaper.pdf", p1, width = 5, height = 4, units = "in")
 
